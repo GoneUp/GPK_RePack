14c18a978b8689580ccc8c6f771f132e95232f46
GoneUp <blauherr@yahoo.de>
2018-09-02 00:48:44 +0200

    started work on a new wpf ui

--- GPK_RePack/Program.cs
+++ GPK_RePack/Program.cs
@@ -15,6 +15,8 @@ namespace GPK_RePack
         [STAThread]
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GUI());
