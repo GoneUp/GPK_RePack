3d902f7e1143ab8cac6f2d7f912992b53494faaa
Foglio1024 <foglio1024@gmail.com>
2020-10-02 14:48:29 +0200

    finished mapper window

--- GPK_RePack_WPF/Windows/MapperWindow.xaml.cs
+++ GPK_RePack_WPF/Windows/MapperWindow.xaml.cs
@@ -6,10 +6,15 @@ using Nostrum.Factories;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
+using NLog;
 
 namespace GPK_RePack_WPF.Windows
 {
@@ -22,7 +27,9 @@ namespace GPK_RePack_WPF.Windows
         {
             InitializeComponent();
 
-            DataContext = new MapperViewModel(gpkStore);
+            var vm = new MapperViewModel(gpkStore);
+            vm.CloseRequested += Close;
+            DataContext = vm;
             MinimizeCommand = new RelayCommand(_ =>
             {
                 WindowState = WindowState.Minimized;
@@ -32,12 +39,21 @@ namespace GPK_RePack_WPF.Windows
             {
                 Close();
             });
-
         }
 
         private void Drag(object sender, MouseButtonEventArgs e)
         {
+            ResizeMode = ResizeMode.NoResize;
             this.TryDragMove();
+            ResizeMode = ResizeMode.CanResize;
+        }
+
+        private void OnSelectedNodeChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue is CompositeMapEntryViewModel vm)
+            {
+                ((MapperViewModel)DataContext).SelectEntry(vm);
+            }
         }
     }
 
@@ -45,10 +61,14 @@ namespace GPK_RePack_WPF.Windows
     {
         private readonly GpkStore gpkStore;
         private readonly DispatcherTimer _searchTimer;
-        private string _searchFilter = "";
+        public static string _searchFilter = "";
+        private readonly Logger _logger;
+        public static event Action<string> SearchUpdated;
+        public event Action CloseRequested;
 
         public TSObservableCollection<CompositeMapEntryViewModel> GpkList { get; }
         public ICollectionView GpkListView { get; }
+        public CompositeMapEntry SelectedEntry { get; set; }
 
         public string SearchFilter
         {
@@ -57,13 +77,12 @@ namespace GPK_RePack_WPF.Windows
             {
                 if (_searchFilter == value) return;
                 _searchFilter = value;
-                _searchTimer.Stop();
-                _searchTimer.Start();
                 N();
+                _searchTimer.Refresh();
             }
         }
 
-        public ICommand DeleteMappingCommand { get; }
+        public ICommand ClearMappingCommand { get; }
         public ICommand DeleteEntryCommand { get; }
 
         public MapperViewModel(GpkStore store)
@@ -73,70 +92,237 @@ namespace GPK_RePack_WPF.Windows
             GpkListView = CollectionViewFactory.CreateCollectionView(GpkList);
             this.gpkStore = store;
             _searchTimer = new DispatcherTimer(TimeSpan.FromMilliseconds(500), DispatcherPriority.Background, OnSearchTimerElapsed, Dispatcher);
+            _logger = LogManager.GetCurrentClassLogger();
 
-            foreach (var pair in gpkStore.CompositeMap)
+            ClearMappingCommand = new RelayCommand(ClearMapping);
+            DeleteEntryCommand = new RelayCommand(DeleteEntry);
+
+            Load();
+        }
+
+        private void ClearMapping()
+        {
+            gpkStore.clearCompositeMap();
+            _logger.Info("Deleted Mapping Cache");
+            CloseRequested?.Invoke();
+        }
+
+        public void SelectEntry(CompositeMapEntryViewModel entry)
+        {
+            SelectedEntry = entry.IsSubGpk ? entry.Source : null;
+        }
+
+
+        private void DeleteEntry()
+        {
+            //if (treeMapperView.SelectedNode == null || treeMapperView.SelectedNode.Tag == null)
+            //    return;
+            if (SelectedEntry == null) return;
+
+            gpkStore.CompositeMap[SelectedEntry.SubGPKName].Remove(SelectedEntry);
+            var targetVm = GpkList.FirstOrDefault(g => g.UID == SelectedEntry.SubGPKName);
+            var targetEntry = targetVm.SubGpkList.FirstOrDefault(sg => sg.Source == SelectedEntry);
+            targetVm?.SubGpkList.Remove(targetEntry);
+            //treeMapperView.OnDrawNodes();
+
+        }
+
+        private void Load()
+        {
+            GpkList.Clear();
+            Task.Run(() =>
             {
-                var vm = new CompositeMapEntryViewModel(pair.Key, pair.Value);
-                GpkList.Add(vm);
-            }
+
+                foreach (var pair in gpkStore.CompositeMap)
+                {
+                    Dispatcher.InvokeAsync(() =>
+                    {
+                        var vm = new CompositeMapEntryViewModel(pair.Key, pair.Value);
+                        vm.SubGpkList.Add(new CompositeMapEntryViewModel("", null));
+                        GpkList.Add(vm);
+                    }, DispatcherPriority.Background);
+                }
+            });
         }
 
         private void OnSearchTimerElapsed(object sender, EventArgs e)
         {
             _searchTimer.Stop();
-            GpkListView.Filter = o => ((CompositeMapEntryViewModel)o).UID.IndexOf(_searchFilter, StringComparison.InvariantCultureIgnoreCase) != -1 
-                                   || ((CompositeMapEntryViewModel)o).SubGpkList.Any(s => s.UID.IndexOf(_searchFilter, StringComparison.InvariantCultureIgnoreCase) != -1);
+            GpkListView.Filter = o => ((CompositeMapEntryViewModel)o).UID.IndexOf(_searchFilter, StringComparison.InvariantCultureIgnoreCase) != -1
+                                   || ((CompositeMapEntryViewModel)o).HasEntry(s => s.UID.IndexOf(_searchFilter, StringComparison.InvariantCultureIgnoreCase) != -1);
             GpkListView.Refresh();
+            SearchUpdated?.Invoke(_searchFilter);
         }
     }
 
 
+    public class GpkTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CompositeGpkDataTemplate { get; set; }
+        public DataTemplate SubGpkDataTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (!(item is CompositeMapEntryViewModel vm)) return null;
 
+            return vm.IsSubGpk ? SubGpkDataTemplate : CompositeGpkDataTemplate;
+        }
+    }
     public class CompositeMapEntryViewModel : TSPropertyChanged
     {
-        public string UID  => IsSubGpk ? _entry.UID : _uid;
+        private bool _isExpanded;
+        private bool _isSubTreeLoaded;
+        private readonly string _uid;
+        private readonly List<CompositeMapEntry> _entries;
+        private bool _isFound;
 
         public bool IsSubGpk { get; }
 
-        private readonly CompositeMapEntry _entry;
-        private readonly string _uid;
-        public List<CompositeMapEntryViewModel> SubGpkList { get; }
+        public string UID => IsSubGpk ? Source?.UID : _uid;
+
+        public string CompositeUID => IsSubGpk ? Source?.CompositeUID : "";
+        public string ParentGPK => IsSubGpk ? Source?.SubGPKName : "";
+        public string FileSize => IsSubGpk
+            ? Source.FileLength >= 1024 * 1024
+                ? $"{Source?.FileLength / (1024 * 1024)} MB"
+                    : Source.FileLength >= 1024
+                    ? $"{Source?.FileLength / 1024} KB"
+                    : $"{Source?.FileLength} B"
+
+                : "";
+
+        public string ContentInfo => IsSubGpk
+            ? ""
+            : _isSubTreeLoaded
+                ? $"({LoadedSubGpkCount}/{SubGpkCount})"
+                : $"({SubGpkCount})";
+
+        public int SubGpkCount => _entries?.Count ?? 0;
+        public int LoadedSubGpkCount => SubGpkList.Count;
+        public TSObservableCollection<CompositeMapEntryViewModel> SubGpkList { get; }
+        public ICollectionViewLiveShaping SubGpkListView { get; }
+        public bool IsSubTreeLoaded
+        {
+            get => _isSubTreeLoaded;
+            set
+            {
+                if (_isSubTreeLoaded == value) return;
+                _isSubTreeLoaded = value;
+                N();
+            }
+        }
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set
+            {
+                if (_isExpanded == value) return;
+                _isExpanded = value;
+                if (_isExpanded)
+                {
+                    if (!IsSubTreeLoaded)
+                    {
+                        LoadSubGpks();
+                        IsSubTreeLoaded = true;
+                    }
+                }
+                else
+                {
+                    IsSubTreeLoaded = false;
+                    ResetSubGpkList();
+                }
+
+                N();
+                ((ICollectionView)SubGpkListView).Refresh();
+            }
+        }
 
         private CompositeMapEntryViewModel()
         {
-            SubGpkList = new List<CompositeMapEntryViewModel>();
-            
+            Dispatcher = Dispatcher.CurrentDispatcher;
+            SubGpkList = new TSObservableCollection<CompositeMapEntryViewModel>();
+            SubGpkListView = CollectionViewFactory.CreateLiveCollectionView(SubGpkList,
+                predicate: o => o.IsVisible,
+                filters: new[] { nameof(IsVisible) },
+                sortFilters: new[] { new SortDescription(nameof(UID), ListSortDirection.Ascending) });
+            MapperViewModel.SearchUpdated += OnSearchUpdated;
+
         }
         public CompositeMapEntryViewModel(string uid, List<CompositeMapEntry> entries) : this()
         {
             _uid = uid;
-            SubGpkList.AddRange(entries.Select(sg => new CompositeMapEntryViewModel(sg)));
-            SubGpkList.Sort(new CompositeMapEntryViewModelComparer());
+            _entries = entries;
             IsSubGpk = false;
-        }
+            OnSearchUpdated(MapperViewModel._searchFilter);
 
+
+        }
         public CompositeMapEntryViewModel(CompositeMapEntry cme) : this()
         {
-            this._entry = cme;
+            this.Source = cme;
             IsSubGpk = true;
+            OnSearchUpdated(MapperViewModel._searchFilter);
         }
-    }
 
-    public class CompositeMapEntryViewModelComparer : IComparer<CompositeMapEntryViewModel>
-    {
-        public int Compare(CompositeMapEntryViewModel x, CompositeMapEntryViewModel y)
+        private void OnSearchUpdated(string obj)
         {
-            return x.UID.CompareTo(y.UID);
+            if (IsSubGpk)
+            {
+                IsFound = obj != "" && this.UID.IndexOf(obj, StringComparison.InvariantCultureIgnoreCase) != -1;
+            }
+        }
+
+
+        public bool IsVisible
+        {
+            get
+            {
+                var ret = MapperViewModel._searchFilter == "" || IsFound || UID == "";
+                return ret;
+            }
+        }
+
+        public bool IsFound
+        {
+            get => _isFound;
+            set
+            {
+                if (_isFound == value) return;
+                _isFound = value;
+                N();
+                N(nameof(IsVisible));
+            }
+        }
+
+        public CompositeMapEntry Source { get; }
+
+        private void ResetSubGpkList()
+        {
+            SubGpkList.Clear();
+            SubGpkList.Add(new CompositeMapEntryViewModel("", null));
+            N(nameof(ContentInfo));
+        }
+        private void LoadSubGpks()
+        {
+            SubGpkList.Clear();
+            Task.Run(() =>
+            {
+                Parallel.ForEach(_entries, entry =>
+                {
+                    Dispatcher.InvokeAsync(() =>
+                    {
+                        var vm = new CompositeMapEntryViewModel(entry);
+                        SubGpkList.Add(vm);
+                        N(nameof(ContentInfo));
+
+                    }, DispatcherPriority.Input);
+                });
+
+            });
+        }
+
+        public bool HasEntry(Func<CompositeMapEntry, bool> func)
+        {
+            return _entries.Any(func);
         }
     }
-
-    //public class CompositeMapEntryViewModel : TSPropertyChanged
-    //{
-
-
-    //    public CompositeMapEntryViewModel(CompositeMapEntry cme)
-    //    {
-    //        this._entry = cme;
-    //    }
-    //}
 }
